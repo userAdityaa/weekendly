59b1ae6c9ad4506a37a673a0970ba3ddabff0b4d
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-09-10 12:50:08 +0530

    feat(constants): Introduce tags constant

---
+++ constants/Tag.ts
@@ -0,0 +1,8 @@
+export const tags = [
+    { name: "BRUNCH", color: "bg-[#fff7d1]" },      // pastel yellow
+    { name: "HIKING", color: "bg-[#fff7d1]" },      // pastel yellow
+    { name: "READING", color: "bg-[#e6f9e6]" },     // pastel green
+    { name: "MOVIE NIGHT", color: "bg-[#ffe6e6]" }, // pastel pink
+    { name: "GARDENING", color: "bg-[#fff3b0]" },   // pastel orange/yellow
+    { name: "JOGGING", color: "bg-[#e6e6fa]" },    // pastel lavender
+  ];
