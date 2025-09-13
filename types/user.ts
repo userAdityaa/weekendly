1c1285f8851817171e5189506a0164c1d50eaaef
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-09-13 18:57:40 +0530

    feat: Add types required for localstorage

--- types/user.ts
+++ types/user.ts
@@ -3,7 +3,39 @@ export interface UserData {
   gender: 'male' | 'female';
   hobby: string;
   totalPlansMade: number;
-  plansList: string[];
+  mainPlanList: MainPlan[];
   friendList: string[];
   locations: string[];
+}
+
+export interface MainPlan {
+  id: string;
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  subPlans: SubPlan[];
+  isPublic: boolean;
+}
+
+export interface SubPlan {
+  id: string;
+  activities: string[];
+  location: string;
+  locationDetails?: {
+    lat: number;
+    lng: number;
+  };
+  friendList: string[];
+  timings: {
+    start: Date | "";
+    end: Date | "";
+  };
+  notes: string;
+}
+
+export interface LocationPoint {
+  name: string;
+  lat: number;
+  lng: number;
+  timesVisited: number;
 }
