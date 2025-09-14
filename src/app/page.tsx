a35df50ce50492551f72a4da11a26879f423696c
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-09-14 22:21:17 +0530

    fix(responsive): For ipads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
           />
 
           {/* Text + Button overlay */}
-          <div className="absolute inset-0 min-2xl:top-[28vh] min-2xl:left-[11vh] top-[16.5rem] left-24 w-[28vw] max-[1182]:w-[29vw] max-[1025]:top-[13rem] max-[1025]:left-20 max-md:w-[90vw] max-md:top-[24vh] max-md:p-4 max-[1181]:top-[15rem] max-[1181]:left-22 max-[391]:left-20">
+          <div className="absolute inset-0 min-2xl:top-[28vh] min-2xl:left-[11vh] top-[16.5rem] left-24 w-[28vw] max-[1182]:w-[29vw] max-[1025]:top-[12rem] max-[1025]:left-[4.15rem] max-md:w-[90vw] max-md:top-[24vh] max-md:p-4 max-[1181]:top-[15rem] max-[1181]:left-22 max-[391]:left-20">
             <div className="flex flex-col justify-center text-black">
               <h1 className="max-[431]:text-3xl max-[391]:text-[1.6rem] text-3xl font-semibold  max-[1025]:text-[1.55rem] min-2xl:text-[3.2vh]">Plan a Perfect Weekend</h1>
               <p className="text-2xl min-2xl:text-[2.8vh] max-[1182]:text-[1.4rem] min-2xl:w-[38vh] max-[1025]:text-[1.15rem] font-medium w-[85%] text-left mt-[1rem] max-[431]:text-2xl max-[391]:text-[1.4rem]">
@@ -66,7 +66,7 @@ export default function Home() {
               {/* Green button full width */}
               <button
                 onClick={handleStartPlanning}
-                className="relative w-[25vw] min-2xl:w-[38vh] min-2xl:h-[8vh] rounded-xl border-2 border-black h-[80px] mt-[1rem] bg-[#82ed82ce] max-md:h-[65px] max-md:w-[80vw] max-md:mt-[0.5rem] max-[391]:w-[80vw] max-[391]:-ml-[0.5rem]"
+                className="relative w-[25vw] min-2xl:w-[38vh] min-2xl:h-[8vh] rounded-xl border-2 border-black h-[80px] mt-[1rem] bg-[#82ed82ce] max-[1025]:h-[4rem] max-[1025]:w-[27vw] max-[1181]:w-[28vw] max-[1367]:w-[26vw] max-md:h-[65px] max-md:w-[80vw] max-md:mt-[0.5rem] max-[391]:w-[80vw] max-[391]:-ml-[0.5rem]"
               >
                 <span className="absolute inset-0 min-2xl:text-[2.5vh] flex items-center justify-center text-black font-semibold text-xl">
                   Start Planning
